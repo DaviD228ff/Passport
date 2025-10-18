7e0988322b77933d8d353d0f5a8791984c0bca0c
Потоковский Михаил <samovar54228@gmail.com>
2025-10-18 14:34:37 +0300

    Initial commit: Passport project

---
+++ Passport.java
@@ -0,0 +1,11 @@
+public class Passport {
+    public static void main(String[] args) {
+        String fullName = "Иванов Иван Иванович";
+        String group = "П-42";
+        String info = "Увлекаюсь программированием и изучаю Java.";
+
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("О себе: " + info);
+    }
+}
